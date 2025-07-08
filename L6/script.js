a470042450d5867c818e524e926d0c77f68ba8ce
Samanyu <samanyusharma444@gmail.com>
2025-07-08 14:21:01 +0530

    Add initial implementation of a Promise example

---
+++ L6/script.js
@@ -0,0 +1,12 @@
+//PROMISE IS AN OBJECT AND HOW DO WE MAKE IT
+
+
+
+const promise1=new Promise((resolve,reject)=>{
+    const a=1+1;
+    if(a==2){
+        resolve("Success");
+    }else{
+        reject("Failed");
+    }
+}
