81a3db9f26ebed07a6a7c5d3a84c667905204b80
Samanyu <samanyusharma444@gmail.com>
2025-07-09 14:34:08 +0530

    9th july work

--- L6/script.js
+++ L6/script.js
@@ -1,12 +1,24 @@
-//PROMISE IS AN OBJECT AND HOW DO WE MAKE IT
+//promise is an object 
 
-
-
-const promise1=new Promise((resolve,reject)=>{
-    const a=1+1;
-    if(a==2){
-        resolve("Success");
-    }else{
-        reject("Failed");
-    }
-});
+const promise1 = new Promise((resolve,reject)=>
+{
+    setTimeout(()=>
+    {
+        resolve("Promise Resolved");
+    },3000)
+})
+promise1
+.then((res)=>console.log(res))
+.then(()=> setTimeout(()=>
+{
+    console.log("p2")
+},2000))
+.then(()=>setTimeout(()=>
+{
+    console.log("p3")
+},5000))
+.then(()=>setTimeout(()=>
+{
+    console.log("p4");
+},1000))
+.catch((err)=>console.log(err));
